082bc0826b1fffc0e407f371d0be9d878fb6b00d
sn_amirhussein <Sn_amirhussein@gmail.com>
2022-08-19 11:10:11 -0700

    deploy react in git

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="portfolio">
         <App />
     </BrowserRouter>
   </React.StrictMode>
