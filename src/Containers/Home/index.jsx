a61efdcdbf3d18aaa9c45e6009c97620b39ff942
sn_amirhussein <Sn_amirhussein@gmail.com>
2022-08-19 11:53:27 -0700

    deploy react in git

--- src/Containers/Home/index.jsx
+++ src/Containers/Home/index.jsx
@@ -31,7 +31,7 @@ const Home = () => {
         }}
       >
           <div className='button__home__div'>
-            <button className='button__home__button'><a href='https://storage.jjcloud.ir/records/files/uploads/documents/df5e0c70-9c19-4363-b121-f8e4d03083c2.pdf?requester=34362e3232352e3232322e313539&resource=753a31393639323634&from=6170706c69636174696f6e5f666f726d&X-Amz-Content-Sha256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=oetSom924MQvtCRu9N6vvQ4bt8cG9HSpRnxWU756SyWy5NVt6nTpmfA9aV2b%2F20220819%2F%2Fs3%2Faws4_request&X-Amz-Date=20220819T174107Z&X-Amz-SignedHeaders=host&X-Amz-Expires=1800&X-Amz-Signature=bf72c064472b5f60063eb36fa2eb2131bf8925bf3fc0cea93a99b6834de2096c'>Resume</a></button>
+            <a href='https://storage.jjcloud.ir/records/files/uploads/documents/df5e0c70-9c19-4363-b121-f8e4d03083c2.pdf?requester=34362e3232352e3232322e313539&resource=753a31393639323634&from=6170706c69636174696f6e5f666f726d&X-Amz-Content-Sha256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=oetSom924MQvtCRu9N6vvQ4bt8cG9HSpRnxWU756SyWy5NVt6nTpmfA9aV2b%2F20220819%2F%2Fs3%2Faws4_request&X-Amz-Date=20220819T174107Z&X-Amz-SignedHeaders=host&X-Amz-Expires=1800&X-Amz-Signature=bf72c064472b5f60063eb36fa2eb2131bf8925bf3fc0cea93a99b6834de2096c' className='button__home__button'>Resume</a>
           </div>
       </Animate>
         </section>
